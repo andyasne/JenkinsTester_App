79f6fd83735078ac943fc33cebd5549d15f04239
andyasne <andyasne@gmail.com>
2016-09-06 16:15:05 +0300

    Initial file upload

---
+++ JenkinsTesterApp/JenkinsTesterApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTesterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Out.Write("This is a test app. Master Branch");
+
+            var intest = System.Console.In.ReadLine();
+        }
+    }
+}
