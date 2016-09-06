41c2c8e8fbc4c9f6b016f4a3d85b5aede1532e99
andyasne <andyasne@gmail.com>
2016-09-06 21:38:20 +0300

    dev Branch

--- JenkinsTesterApp/JenkinsTesterApp/Program.cs
+++ JenkinsTesterApp/JenkinsTesterApp/Program.cs
@@ -10,7 +10,7 @@ namespace JenkinsTesterApp
     {
         static void Main(string[] args)
         {
-            System.Console.Out.Write("This is a test app. Master Branch");
+            System.Console.Out.Write("This is a test app. Development Branch");
 
             var intest = System.Console.In.ReadLine();
         }
