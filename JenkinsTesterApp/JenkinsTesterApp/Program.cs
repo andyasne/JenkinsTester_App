da1208dd07cf1bb10c13f8e3bd881018314c212e
andyasne <andyasne@gmail.com>
2016-09-08 20:34:08 +0300

    chheck

--- JenkinsTesterApp/JenkinsTesterApp/Program.cs
+++ JenkinsTesterApp/JenkinsTesterApp/Program.cs
@@ -10,7 +10,7 @@ namespace JenkinsTesterApp
     {
         static void Main(string[] args)
         {
-            System.Console.Out.Write("This is a test app.Development Branch:Hook checking!");
+            System.Console.Out.Write("This is a test app.Development Branch:Hook checking!__");
 
             var intest = System.Console.In.ReadLine();
         }
