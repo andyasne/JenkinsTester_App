3570f3f9ff178ec4bd3d3f5dfd1663022af03017
andyasne <andyasne@gmail.com>
2016-09-08 20:49:29 +0300

    testing Hook

--- JenkinsTesterApp/JenkinsTesterApp/Program.cs
+++ JenkinsTesterApp/JenkinsTesterApp/Program.cs
@@ -10,7 +10,7 @@ namespace JenkinsTesterApp
     {
         static void Main(string[] args)
         {
-            System.Console.Out.Write("This is a test app. Master Branch_feature");
+            System.Console.Out.Write("This is a test app. Master Branch_fea");
 
             var intest = System.Console.In.ReadLine();
 
